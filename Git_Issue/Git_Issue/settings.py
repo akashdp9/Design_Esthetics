82579d87d76f50f9ded371652a3d5aadb1a5bca9
Akashdeep <akashdp9@gmail.com>
2020-02-27 15:42:36 +0530

    Complete backend

--- Git_Issue/Git_Issue/settings.py
+++ Git_Issue/Git_Issue/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Issues',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'Git_Issue.urls'
@@ -113,6 +118,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Static files (CSS, JavaScript, Images)
---
+++ Git_Issue/Issues/serializer.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Categories,Items
+
+
+class CategoriesSerializers(serializers.ModelSerializer):
+    
+
+    class Meta:
+        model = Categories
+        fields = ('id','name',)
+
+
+
+        
+class ItemsSerializers(serializers.ModelSerializer):
+    type_of = CategoriesSerializers()
+    
+
+    class Meta:
+        model = Items
+        fields = ('id','type_of','description',)
+
+
