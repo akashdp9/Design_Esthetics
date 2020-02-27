5e9a528777631fb5963706032130eb92c9e8d13a
Akashdeep <akashdp9@gmail.com>
2020-02-27 15:25:24 +0530

    admin registered

--- Git_Issue/Issues/admin.py
+++ Git_Issue/Issues/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categories,Items
 
-# Register your models here.
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+
+
+admin.site.register(Categories, CategoriesAdmin)
+
+class ItemsAdmin(admin.ModelAdmin):
+    list_display = ['type_of','description',]
+
+
+admin.site.register(Items, ItemsAdmin)
